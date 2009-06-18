4654a082a859968f4591970484a31d9fabb44420
Florian Tobias Schandinat <FlorianSchandinat@gmx.de>
2009-06-18 16:51:58 +0200

    viafb: remove dead module parameter and code
    viafb: remove dead module parameter and code
    
    Signed-off-by: Florian Tobias Schandinat <FlorianSchandinat@gmx.de>

--- iface.h
+++ iface.h
@@ -22,8 +22,7 @@
 #ifndef __IFACE_H__
 #define __IFACE_H__
 
-#define Kb  (1024)
-#define Mb  (Kb*Kb)
+#define Mb  (1024*1024)
 
 #define VIA_K800_BRIDGE_VID         0x1106
 #define VIA_K800_BRIDGE_DID         0x3204
@@ -31,8 +30,6 @@
 #define VIA_K800_SYSTEM_MEMORY_REG  0x47
 #define VIA_K800_VIDEO_MEMORY_REG   0xA1
 
-extern int viafb_memsize;
-unsigned int viafb_get_memsize(void);
 unsigned long viafb_get_videobuf_addr(void);
 
 #endif /* __IFACE_H__ */
